2d8bcc35a553c5f812dc2868eedc294f86bc9002
momohudsonbarton <160808466+momohudsonbarton@users.noreply.github.com>
2024-05-21 17:46:51 +1000

    Update publishedEmpiricalThreshes.py
    updating for one target

--- theory/publishedEmpiricalThreshes.py
+++ theory/publishedEmpiricalThreshes.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-threshHzPrevLit = { 'numTargets': [2,3],
-                    'HzRoudaiaFaubertYoungMen': [4, 2.8],
-                    'HzRoudaiaFaubertYoungWomen': [3, 1.8],
-                    'HzHolcombeChenE1': [4.45, None],
-                    'HzHolcombeChenE2': [4.05,2.7]       }
+threshHzPrevLit = { 'numTargets': [1,2,3],
+                    'HzRoudaiaFaubertYoungMen': [6.4, 4, 2.8],
+                    'HzRoudaiaFaubertYoungWomen': [4.9, 3, 1.8],
+                    'HzHolcombeChenE1': [7.1, 4.45, None],
+                    'HzHolcombeChenE2': [None,4.05,2.7]       }
 	#ADD  1 target including MARINOVIC finding of 7.2 Hz for one target, and Verstraten
 	#If add old people findings, make sure adjust code in MOTcircular.py that uses for starting point of staircase
 
